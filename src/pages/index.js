3dc416fdd39a2aac8ca8413d40928436a8435ea8
sipun05 <sipunkumarpanda.05@gmail.com>
2025-04-06 04:51:31 +0530

    Updated User Landing Page and Event Calender

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import Home from "./Home";
 import AuthForm from "./AuthForm";
 import Events from "./Events";
+import CreateAnPage from "./EventCalender";
+
 
 const router = [
   { path: "/", component: Home },
   { path: "/login", component: AuthForm },
   { path: "/events", component: Events },
+  {path:"/orgdet",component:CreateAnPage}
+  // Lazy load the Emoji component
+
 ];
 
 export default router;
