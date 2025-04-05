9d73c9de673bfa7ebf2498c5fe85a007dc655716
SaiTejaSalvaji <saitejasalvaji1201@gmail.com>
2025-04-05 18:14:38 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
+import Header from "./components/Header"; // make sure path is correct
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <Header />
+      <div className="pt-20"> {/* To avoid overlap due to fixed header */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
