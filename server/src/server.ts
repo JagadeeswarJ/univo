5f8fbf3ed4913d427892f72bc54f94d436bc1acf
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-07-09 12:42:50 +0530

    reoreder routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,14 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//new routes
-app.use("/", (req, res) => {
-  res.send("Univo Server")
-});
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use("/event", EventRoute);
 app.use("/payment", PaymentRoute);
+//new routes
+app.use("/", (req, res) => {
+  res.send("Univo Server")
+});
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ success: false, message: err.message });
